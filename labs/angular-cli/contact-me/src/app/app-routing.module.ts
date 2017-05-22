e7fe9ed38f8cb4fc32fa3ee6a44b157ef72ee914
Jason Swett <jason@benfranklinlabs.com>
2017-05-22 19:39:05 -0400

    Add routing.

--- labs/angular-cli/contact-me/src/app/app-routing.module.ts
+++ labs/angular-cli/contact-me/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'contacts',
+    component: ContactListComponent
   }
 ];
 
