623904a46cff89583fed1a259a091c37c605c7aa
Jason Swett <jason@benfranklinlabs.com>
2017-05-22 18:35:48 -0400

    Add LocalStorage.

--- labs/angular-cli/contact-me/src/app/contact.service.ts
+++ labs/angular-cli/contact-me/src/app/contact.service.ts
@@ -2,24 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ContactService {
-  contacts = [
-    {
-      name: 'Paul McCartney',
-      phone: '(683) 385-3835'
-    },
-    {
-      name: 'John Lennon',
-      phone: '(322) 395-3388'
-    }
-  ];
-
   constructor() { }
 
   save(contact) {
-    this.contacts.push(contact);
+    let contacts = this.getList();
+    contacts.push(contact);
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }
 
   getList() {
-    return this.contacts;
+    return JSON.parse(localStorage.getItem('contacts')) || [];
   }
 }
