ccaaeabc26bdbaa6cd1f4078d424c377b6b60ed7
Jason Swett <jason@benfranklinlabs.com>
2017-05-23 18:52:47 -0400

    Get new searcher working.

--- labs/wikipedia-search/searcher/src/app/app.component.ts
+++ labs/wikipedia-search/searcher/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WikipediaService]
 })
 export class AppComponent {
-  title = 'app works!';
+  items: Array<string>;
+
+  constructor(private wikipediaService: WikipediaService) { }
+
+  search(term: string) {
+    this.wikipediaService.search(term)
+      .subscribe(items => this.items = items);
+  }
 }
