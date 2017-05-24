a3f5d5be10288a50113123f9e674022d52021716
Jason Swett <jason@benfranklinlabs.com>
2017-05-24 06:25:59 -0400

    Add test for ContactService.save().

--- labs/angular-cli/contact-me/src/app/contact.service.spec.ts
+++ labs/angular-cli/contact-me/src/app/contact.service.spec.ts
@@ -11,4 +11,20 @@ describe('ContactService', () => {
   it('should be created', inject([ContactService], (service: ContactService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('save', function() {
+    it('should save a contact', inject([ContactService], (service: ContactService) => {
+      service.deleteAll();
+
+      let contact = {
+        name: 'Jason Swett',
+        slug: 'jason-swett',
+        phone: '(616) 856-8075'
+      };
+
+      service.save(contact);
+
+      expect(service.getList()).toContain(contact);
+    }));
+  });
 });
